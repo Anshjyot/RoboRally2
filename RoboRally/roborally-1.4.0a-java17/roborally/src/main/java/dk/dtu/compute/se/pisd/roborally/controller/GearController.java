5eac495824cad0457c7491ce34dd1b3890d6b3e5
NickTahmasebi <nicktahmasebi@hotmail.com>
2022-06-03 14:28:51 +0200

    GearController1

--- RoboRally/roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GearController.java
+++ RoboRally/roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GearController.java
@@ -1,9 +1,11 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
 
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Player;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 
 public class GearController extends FieldAction {
+
     @Override
     public boolean doAction(GameController gameController, Space space) {
         Player currentPlayer = space.getPlayer();
